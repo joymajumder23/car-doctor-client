f000de6d39a8fa2b0840f467a95c2c2e5fa0161a
joymajumder23 <joymajumderjishu@gmail.com>
2024-05-02 23:42:23 +0600

    services section adding

---
+++ src/pages/Home/Sevices/Services.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import ServiceCard from "./ServiceCard";
+
+const Services = () => {
+    const [services, setServices] = useState([]);
+    useEffect(() => {
+        fetch('./services.json')
+        .then(res => res.json())
+        .then(data => setServices(data))
+    },[])
+    console.log(services);
+    return (
+        <div className="mt-24">
+            <div className="text-center space-y-5">
+                <h1 className="text-xl font-bold text-orange-600">Our Services</h1>
+                <h1 className="text-5xl font-bold">Our Service Area</h1>
+                <p className="text-slate-500">The majority have suffered alteration in some form, by injected humour, or randomised <br /> words which do not look even slightly believable.</p>
+            </div>
+
+            <div>
+                {
+                    services.map(service => <ServiceCard key={service._id} service={service}></ServiceCard>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Services;
