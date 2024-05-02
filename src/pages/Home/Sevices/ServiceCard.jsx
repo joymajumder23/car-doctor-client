5e1b699544afc6108a5cbbb2ac0b675f5993e33c
joymajumder23 <joymajumderjishu@gmail.com>
2024-05-03 01:41:49 +0600

    services section added

--- src/pages/Home/Sevices/ServiceCard.jsx
+++ src/pages/Home/Sevices/ServiceCard.jsx
@@ -1,16 +1,19 @@
+import { FaArrowRight } from "react-icons/fa";
+
 const ServiceCard = ({service}) => {
-    const {img} = service;
+    const {img, title, price} = service;
     return (
-        <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card w-96 bg-base-100 shadow-xl border">
             <figure className="px-10 pt-10">
-                <img src={img} alt="Shoes" className="rounded-xl" />
+                <img src={img} alt="" className="rounded-xl" />
             </figure>
-            <div className="card-body items-center text-center">
-                <h2 className="card-title">Shoes!</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div className="card-actions">
-                    <button className="btn btn-primary">Buy Now</button>
+            <div className="card-body">
+                <h2 className="card-title text-2xl font-bold">{title}</h2>
+                <div className="flex justify-between items-center">
+                <p className="text-xl font-semibold text-orange-600">Price: ${price}</p>
+                <button className="btn bg-transparent border-none shadow-none text-orange-600 text-xl"><FaArrowRight></FaArrowRight></button>
                 </div>
+                
             </div>
         </div>
     );
--- src/pages/Home/Sevices/Services.jsx
+++ src/pages/Home/Sevices/Services.jsx
@@ -17,7 +17,7 @@ const Services = () => {
                 <p className="text-slate-500">The majority have suffered alteration in some form, by injected humour, or randomised <br /> words which do not look even slightly believable.</p>
             </div>
 
-            <div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-12">
                 {
                     services.map(service => <ServiceCard key={service._id} service={service}></ServiceCard>)
                 }
