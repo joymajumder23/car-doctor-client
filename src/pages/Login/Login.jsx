ab40cbb238e87eaad684f3417ab3887b002fe28f
joymajumder23 <joymajumderjishu@gmail.com>
2024-05-03 03:15:34 +0600

    login page added and firebase added

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,45 @@
+import login from "../../assets/images/login/login.svg";
+
+const Login = () => {
+    const handleLogin = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+    }
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row gap-10">
+                <div className="text-center lg:text-left w-1/2">
+                   <img src={login} alt="" />
+                </div>
+                <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form onSubmit={handleLogin} className="card-body border-2 rounded-xl">
+                    <h1 className="text-4xl font-semibold text-center">Login</h1>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" placeholder="email" className="input input-bordered" name="email" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <input className="btn bg-orange-600 text-white" type="submit" value="Login" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,17 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
 import Home from "../pages/Home/Home/Home";
+import Login from "../pages/Login/Login";
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main></Main>,
-      children: [
-        {
-            path: "/",
-            element: <Home></Home>
-        }
-      ]
-    },
-  ]);
-  export default router;
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+      {
+        path: "/login",
+        element: <Login></Login>
+      }
+    ]
+  },
+]);
+export default router;
